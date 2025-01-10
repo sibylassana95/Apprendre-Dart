0d28f1f67dc1e99544d7fdb5657bef3ee01c006b
sibylassana95 <sibyamara95@gmail.com>
2025-01-10 15:41:15 +0000

    Ajout d'un programme pour déterminer si un nombre est pair ou impair en Dart

---
+++ exercice1.dart
@@ -0,0 +1,9 @@
+void main() {
+  /*   Écris un programme qui détermine si un nombre donné est pair ou impair. */
+  int nombre = 5;
+  if (nombre % 2 == 0) {
+    print("$nombre est pair");
+  } else {
+    print("$nombre est impair");
+  }
+}
