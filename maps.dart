c40417cec56509e5df2dc86ebf72554d037aafc2
sibylassana95 <sibyamara95@gmail.com>
2025-01-10 13:42:12 +0000

    Ajout d'un programme pour manipuler des maps en Dart

---
+++ maps.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("----------------------------Maps");
+  Map<String, String> fruits = {
+    "pomme": "rouge",
+    "banane": "jaune",
+    "orange": "orange",
+  };
+  print(fruits); // Affiche la map
+
+  print(fruits["pomme"]); // Accès à une valeur
+}
