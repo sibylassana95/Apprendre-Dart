597dec072434cc1f321105524881ae5af81ad2f7
sibylassana95 <sibyamara95@gmail.com>
2025-01-10 13:41:45 +0000

    Ajout d'un programme pour effectuer des opérations arithmétiques et de comparaison de base

---
+++ operation.dart
@@ -0,0 +1,21 @@
+void main() {
+  print("----------------------------Opérations ");
+  // Opérations arithmétiques de base
+  int a = 10;
+  int b = 5;
+  print(a + b); // Addition
+  print(a - b); // Soustraction
+  print(a * b); // Multiplication
+  print(a / b); // Division
+
+  // Opérations de comparaison de base
+  print(a > b); // Supérieur
+  print(a < b); // Inférieur
+  print(a >= b); // Supérieur ou égal
+  print(a <= b); // Inférieur ou égal
+  print(a == b); // Égal
+  print(a != b); // Différent
+  print(a > b && a < b); // ET
+  print(a > b || a < b); // OU
+  print(!(a > b)); // NON
+}
