5b22939b8501e70f2d333db8a9e2d21f4cd0f061
sibylassana95 <sibyamara95@gmail.com>
2025-01-10 13:41:38 +0000

    Ajout d'un programme de base pour afficher des informations personnelles

---
+++ variable.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'John';
+  int age = 25;
+  double height = 1.75;
+  bool isStudent = true;
+  print(
+    "je m'appelle $name, j'ai $age ans, je mesure $height mètre , je suis un étudiant $isStudent",
+  );
+}
