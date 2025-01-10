5a0c145ff80c950129070d13116a877dde8dc59a
sibylassana95 <sibyamara95@gmail.com>
2025-01-10 15:41:21 +0000

    Ajout d'un programme pour comparer deux nombres et afficher le plus grand en Dart

---
+++ exercice2.dart
@@ -0,0 +1,10 @@
+void main() {
+  /*  Demande à l’utilisateur de saisir deux nombres, puis affiche le plus grand des deux. */
+  int nombre1 = 5;
+  int nombre2 = 3;
+  if (nombre1 > nombre2) {
+    print("$nombre1 est plus grand que $nombre2");
+  } else {
+    print("$nombre2 est plus grand que $nombre1");
+  }
+}
